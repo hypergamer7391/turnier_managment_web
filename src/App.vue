e8435ac2fb9928bae9011cf1e13bd2f1d7d7ec8e
Julius Weiling <juliusweiling@iMac-von-Julius.local>
2025-07-06 20:19:17 +0200

    stats

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import AppFooter from './components/AppFooter.vue'
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
   <RouterView :key="$route.url"/>
-  
+  <Analytics />
 </template>
 
 <style scoped>
